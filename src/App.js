37c3d56fba8d4b11576f8b8dc6aac5bed8842d95
Amala <Amala@LAPTOP-EMU5LRFA>
2024-03-28 20:20:33 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -4,32 +4,33 @@ import axios from 'axios';
 
 function App() {
   const [inputData, setInputData] = useState('');
-  const [prediction, setPrediction] = useState('');
+  const [prediction, setPrediction] = useState(NaN);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  //const [message,setMessage]=useState('');
+  var message = '';
 
   const predict = async () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.post('http://your-api-url/predict', {
-        data: inputData
-      });
-      setPrediction(response.data.prediction);
+      console.log(inputData);
+      const response = await axios.get(`http://127.0.0.1:8000/rainfall/${inputData}`);
+      console.log(response);
+      setPrediction(response.data.data[0]);
+      console.log(prediction);
     } catch (error) {
       setError('An error occurred. Please try again later.');
     }
     setLoading(false);
   };
-  /*if(prediction)
+  if(prediction)
   {
-    setMessage("A disaster will occur");
+    message = "A disaster will occur"
   }
   else
   {
-    setMessage("A disaster will not occur");
-  }*/
+    message = "A disaster will not occur"
+  }
  
   return (
     <div className="App">
@@ -44,17 +45,30 @@ function App() {
       <br></br><br></br><h1>SELECT A CITY:</h1>
       <div className="App-body">
         <select value={inputData} onChange={e => setInputData(e.target.value)}>
+          <option value="None">None</option>
           <option value="Chennai">Chennai</option>
           <option value="Mayiladuthurai">Mayiladuthurai</option>
           <option value="Thoothukudi">Thoothukudi</option>
           <option value="Nagercoil">Nagercoil</option>
+          <option value="Thiruvananthapuram">Thiruvananthapuram</option>
+          <option value="Kollam">Kollam</option>
+          <option value="Kozhikode">Kozhikode</option>
+          <option value="Kannur">Kannur</option>
+          <option value="Visakhapatnam">Visakhapatnam</option>
+          <option value="Nellore">Nellore</option>
+          <option value="Mangaluru">Mangaluru</option>
+          <option value="Udupi">Udupi</option>
+          <option value="Mumbai">Mumbai</option>
+          <option value="Daman">Daman</option>
+          <option value="Alappuzha">Alappuzha</option>
+          <option value="Kakinada">Kakinada</option>
         </select><br></br><br></br>
 
 
         <button onClick={predict}>Predict</button>
         {loading && <p className="loading">Loading...</p>}
         {error && <p className="error">{error}</p>}
-        {prediction && <p className="prediction">Prediction: {prediction}</p>}
+        {!isNaN(prediction) ? <p className="prediction">Prediction: {message}</p> : <p></p>}
       </div><br></br><br></br><br></br><br></br><br></br>
       <marquee behavior="scroll" direction="left"><b>NATIONAL EMERGENCY NUMBER:112&nbsp;&nbsp;&nbsp;&nbsp;DISASTER MANAGEMENT:1078,01126701728&nbsp;&nbsp;&nbsp;&nbsp;NDRF HELPLINE NUMBER:9711077372,011-24363260&nbsp;&nbsp;&nbsp;&nbsp;AIDS HELPLINE:1097&nbsp;&nbsp;&nbsp;&nbsp;NATIONAL EMERGENCY NUMBER:112&nbsp;&nbsp;&nbsp;&nbsp;DISASTER MANAGEMENT:1078,01126701728&nbsp;&nbsp;&nbsp;&nbsp;NDRF HELPLINE NUMBER:9711077372,011-24363260&nbsp;&nbsp;&nbsp;&nbsp;AIDS HELPLINE:1097</b></marquee>
       <footer className="App-footer">
